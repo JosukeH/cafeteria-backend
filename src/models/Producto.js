21a66df3b5138ccbf98850ff4b95100da14c109b
JosukeH <solodcarlos@gmail.com>
2022-12-01 16:29:24 -0600

    a4

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -3,5 +3,14 @@ import mongoose from 'mongoose'
 const { model, Schema } = mongoose
 
 const ProductoSchema = Schema({
-
+  id: Number,
+  nombre: String,
+  precio: Number,
+  tipo: String,
+  Categoria: String,
+  idReceta: Number
 })
+
+const Producto = model('Producto', ProductoSchema)
+
+export default Producto
