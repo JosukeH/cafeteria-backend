4efa744f6c56f9f1596bfc6f802693a5b1af08f7
¨JosukeH¨ <solodcarlos@gmail.com>
2022-12-04 11:10:05 -0600

    axd

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -7,8 +7,13 @@ const ProductoSchema = Schema({
   nombre: String,
   precio: Number,
   tipo: String,
-  Categoria: String,
-  idReceta: Number
+  categoria: String,
+  receta: [{
+    id: Number,
+    ingrediente: String,
+    cantidad: Number
+  }]
+
 })
 
 const Producto = model('Producto', ProductoSchema)
